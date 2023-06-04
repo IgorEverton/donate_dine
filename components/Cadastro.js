c1d6bcaf6f7ffbf07f7a8d6a7b231864dd63d0f2
IgorEverton <evertonalves369@gmail.com>
2023-06-04 13:48:41 -0300

    tela de login e cadastro adicionado

---
+++ components/Cadastro.js
@@ -0,0 +1,72 @@
+import React,  {useState} from 'react';
+import {Text, View, TextInput, TouchableOpacity} from 'react-native';
+
+  const estilos = {
+    h1:{
+      textAlign:"center",
+      fontSize:35, fontWeight:"bold",
+      marginTop:"40px", marginBottom:"40px"
+    },
+    input:{
+      backgroundColor:"white",
+      height:"30px", width:"250px",
+      borderRadius:"5px", 
+      marginTop:"5px",  paddingLeft: 10,
+      fontSize:"13px", fontWeight: 300,
+      letterSpacing: 1.5
+    },
+    botaoCadastrar:{
+      backgroundColor:"#0B8700",  color:"white",
+      width:"180px", height:"40px", 
+      borderRadius:"5px", 
+      justifyContent:"center", alignItems:"center", 
+      marginVertical: 50,marginBottom:"5%"
+    }
+  }
+
+function InputText(props){
+
+  return(
+    <>
+      <View style={{marginTop:"35px"}}>
+        <Text style={{fontSize:15, fontWeight: 500, paddingLeft: 10,color:"white"}}>{props.titulo}</Text>
+        <TextInput placeholder={props.placeholder} style={estilos.input}/>
+      </View>
+    </>
+  )
+}
+
+const CadastroForm = () =>{
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmaSenha, setconfirmaSenha] = useState("");
+
+      return(
+      <View style={{display:"flex",flex:1, margin:25}}>
+        <View style={{alignItems:"center"}}>
+        <Text style={{textAlign:"center",fontSize:43, fontWeight:"bold",marginTop:"80px",color:"white"}}>Cadastro</Text>
+          <InputText titulo={"Nome"}/>
+          <InputText titulo={"E-mail"}/>
+          <InputText titulo={"Nova senha"}/>
+          <InputText titulo={"Confime sua senha"}/>
+            <TouchableOpacity style={estilos.botaoCadastrar}>
+                <Text style={{color:"white", fontSize:18 }}>Cadastrar</Text>
+            </TouchableOpacity>
+            <Text style={{color:"#3DA2FF",marginTop:"32px",fontWeight: 600}}>
+              <TouchableOpacity>Voltar</TouchableOpacity>
+            </Text>
+        </View>
+      </View>
+  )
+  }
+
+
+
+export default ()=>{
+  return(
+    <View style={{flex:1, backgroundColor:"black"}}> 
+      <CadastroForm/>
+    </View>
+  )
+}
---
+++ components/Login.js
@@ -0,0 +1,56 @@
+import React,  {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Text, View, TextInput, TouchableOpacity} from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import Cadastro from './Cadastro';
+
+const LoginForm = () =>{
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    async function salvarAuthToken() {
+      await AsyncStorage.setItem('authToken', authToken);
+    }
+
+    salvarAuthToken();
+  }, []);
+
+      return(
+      <View style={{display:"flex",flex:1, margin:25,alignItems:"center", backgroundColor:"black"}}>
+        
+        <Text style={{textAlign:"center",fontSize:37, fontWeight:"bold",marginTop:"180px", marginBottom:"30px", color:"white"}}>DONATE DINE</Text>
+
+        <View style={{marginTop:"35px"}}>
+          <Text style={{fontSize:15, fontWeight:"bold", color:"white"}}>E-mail</Text>
+          <TextInput onChangeText={setEmail} style={{backgroundColor:"white", height:"30px", width:"250px",borderRadius:"5px"}}/>
+        </View>
+
+        <View style={{flexDirection:"row",justifyContent:"space-between",marginTop:"32px",width:"250px"}}>
+          <Text style={{fontSize:15, fontWeight:"bold", color:"white"}}>Senha</Text>       
+          <TouchableOpacity><Text style={{color:"#3DA2FF"}}>Esqueceu sua senha?</Text></TouchableOpacity>
+
+        </View>
+          <TextInput onChangeText={setSenha} style={{backgroundColor:"white", height:"30px", width:"250px",borderRadius:"5px",marginTop:"5px", marginBottom:"40px"}}/>
+            <TouchableOpacity style={{backgroundColor:"#0B8700", width:"110px", height:"40px", borderRadius:"5px", justifyContent:"center", alignItems:"center", color:"white"}}>
+                <Text style={{color:"white", fontSize:18 }}>Logar</Text>
+            </TouchableOpacity>  
+            <Text style={{fontWeight:200, color:"#3DA2FF",marginTop:"32px"}}>Não tem conta? 
+              <TouchableOpacity onPress={() => navigation.navigate(Cadastro)} style={{ fontWeight: 600}}> Se cadastre clicando aqui</TouchableOpacity>
+            </Text>
+      </View>
+  )
+  }
+
+
+
+export default ()=>{
+  return(
+    <NavigationContainer>
+      <View style={{flex:1, backgroundColor:"black"}}> 
+        <LoginForm/>
+      </View>
+    </NavigationContainer>
+  )
+}
