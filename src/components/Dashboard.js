4bcdbb07646fdce7f7070407d4a038ad93c5f563
IgorEverton <evertonalves369@gmail.com>
2023-06-06 11:17:26 -0300

    disposição da tela

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,53 +1,48 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-
-const {Navigator, Screen} = createBottomTabNavigator();
 
 const RetornoDash=()=>{
   return(
       <View style={{flex:1, backgroundColor:"black", alignItems:"center"}}>
       <Text style={{fontSize:38, fontWeight:"bold",marginTop:20,color:"white",marginBottom: 53}}>DashBoard</Text>
-      <View style={{borderWidth:2, borderColor:"white", width:310, height:205,alignItems:"center",borderRadius:10,marginBottom:20}}>
-        <AnimatedCircularProgress style={{marginTop:18}}
+      <View style={{borderWidth:2, borderColor:"white", width:310, height:205,alignItems:"center",borderRadius:10,marginBottom:36}}>
+        <AnimatedCircularProgress style={{marginTop:18, marginBottom:10}}
           size={120}
           width={25}
           fill={70}
           tintColor="#00e0ff"
           onAnimationComplete={() => console.log('onAnimationComplete')}
           backgroundColor="#3d5875" />
-      <View style={{flexDirection: "row", marginLeft: 30, width: "100%"}}>
-        <View style={{height: 15, width:15, backgroundColor:"#00e0ff"}}></View>
-        <Text style={{color:"white", marginRight:80, marginBottom: 5}}> Total de lotes cadastrados</Text>
+        <View style={{flexDirection: "row", marginLeft: 30, width: "100%"}}>
+          <View style={{height: 15, width:15, backgroundColor:"#00e0ff"}}></View>
+          <Text style={{color:"white", marginRight:80, marginBottom: 5}}> Total de lotes cadastrados</Text>
+        </View>
+        <View style={{flexDirection: "row", marginLeft: 30, width: "100%"}}>
+          <View style={{height: 15, width:15, backgroundColor:"#3d5875"}}></View>
+          <Text style={{color:"white", marginRight:80, marginBottom: 5}}> Total de lotes doados</Text>
+        </View>
       </View>
-      <View style={{flexDirection: "row", marginLeft: 30, width: "100%"}}>
-        <View style={{height: 15, width:15, backgroundColor:"#3d5875"}}></View>
-        <Text style={{color:"white", marginRight:80, marginBottom: 5}}> Total de lotes doados</Text>
-      </View>
-        <Text style={{fontSize:20, fontWeight:"bold", color:"white", marginTop:5,marginBottom: 60}}>Lotes Doados</Text>
-      </View>
-      <View style={{borderWidth:2, borderColor:"white", width:310, height:205,alignItems:"center",borderRadius:10,marginBottom:20}}>
-        <AnimatedCircularProgress style={{marginTop:18}}
+      <View style={{borderWidth:2, borderColor:"white", width:310, height:205,alignItems:"center",borderRadius:10,marginBottom:36}}>
+        <AnimatedCircularProgress style={{marginTop:18, marginBottom:10}}
           size={120}
           width={25}
           fill={30}
           tintColor="#00e0ff"
           onAnimationComplete={() => console.log('onAnimationComplete')}
           backgroundColor="#3d5875" />
-      <View style={{flexDirection: "row", marginLeft: 30, width: "100%"}}>
-        <View style={{height: 15, width:15, backgroundColor:"#00e0ff"}}></View>
-        <Text style={{color:"white", marginRight:80, marginBottom: 5}}> Total de lotes cadastrados</Text>
+        <View style={{flexDirection: "row", marginLeft: 30, width: "100%"}}>
+          <View style={{height: 15, width:15, backgroundColor:"#00e0ff"}}></View>
+          <Text style={{color:"white", marginRight:80, marginBottom: 5}}> Total de lotes cadastrados</Text>
+        </View>
+        <View style={{flexDirection: "row", marginLeft: 30, width: "100%"}}>
+          <View style={{height: 15, width:15, backgroundColor:"#3d5875"}}></View>
+          <Text style={{color:"white", marginRight:80, marginBottom: 5}}> Total de lotes doados</Text>
+        </View>
       </View>
-      <View style={{flexDirection: "row", marginLeft: 30, width: "100%"}}>
-        <View style={{height: 15, width:15, backgroundColor:"#3d5875"}}></View>
-        <Text style={{color:"white", marginRight:80, marginBottom: 5}}> Total de lotes doados</Text>
-      </View>
-        <Text style={{fontSize:20, fontWeight:"bold", color:"white", marginTop:5,marginBottom: 60}}>Ocupação do estoque</Text>
-      </View>
-      <View style={{borderWidth:2, borderColor:"white", width:310, height:205,alignItems:"center",borderRadius:10,marginBottom:20}}>
-        <AnimatedCircularProgress style={{marginTop:18}}
+      <View style={{borderWidth:2, borderColor:"white", width:310, height:205,alignItems:"center",borderRadius:10,marginBottom:36}}>
+        <AnimatedCircularProgress style={{marginTop:18, marginBottom:10}}
           size={120}
           width={25}
           fill={90}
